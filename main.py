622b90a7828f5f6a69c03afa0b57c9c4ff176113
Karim <abdulkareemx2@gmail.com>
2021-07-05 13:59:47 +0900

    fixed channels none bug and removed tracing comments

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ todays_number = 1
 
 @tasks.loop(minutes=1)
 async def called_once_a_day():
-    print("called once a day")
-    message_channel = bot.get_channel(os.environ['TARGET_CHANNEL_ID'])
-    print("INFO: message_channel is "+ str(os.environ['TARGET_CHANNEL_ID']))
+    channel_id = int(os.environ['TARGET_CHANNEL_ID']) # needs to be cast to int otherwise would return none
+    message_channel = bot.get_channel(channel_id)
     global todays_number
     print(f"Sending day {todays_number} on channel {message_channel}")
-    kanji_of_the_day_markdown = kanji_reader.generate_todays_kanji_(todays_number, CONFIG.KANJI_AMOUNT)
     await message_channel.send(file=discord.File('Todays_Kanji.png'))
-    todays_number = todays_number + 1
+    kanji_of_the_day_markdown = kanji_reader.generate_todays_kanji_(todays_number, CONFIG.KANJI_AMOUNT)
     await message_channel.send(kanji_of_the_day_markdown)
+    todays_number = todays_number + 1
 
 @called_once_a_day.before_loop
 async def before():
@@ -26,5 +25,4 @@ async def before():
     print("Bot is running")
 
 called_once_a_day.start()
-print("INFO: token is "+ str(os.environ['DISCORD_BOT_TOKEN']))
 bot.run(os.environ['DISCORD_BOT_TOKEN'])

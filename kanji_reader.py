2def5976ec0f0eab784a62b187897512cd594538
Karim <abdulkareemx2@gmail.com>
2021-07-06 08:29:09 +0900

    clean up and ready for prod

--- kanji_reader.py
+++ kanji_reader.py
@@ -11,14 +11,9 @@ n1_df = n1_df[["strokes","grade","freq","meanings","readings_on","readings_kun"]
 def generate_todays_kanji_(todays_day_number, KANJI_AMOUNT):
     starting_kanji_index = (todays_day_number - 1) * KANJI_AMOUNT
     kanjis_of_the_day_df = n1_df.iloc[starting_kanji_index:starting_kanji_index + KANJI_AMOUNT]
-    #dfi.export(kanjis_of_the_day_df, "Todays_Kanji.png")
-    html = kanjis_of_the_day_df.to_html()
-    #imgkit.from_string(html, "Todays_Kanji_Table.png")
     kanji_and_meanings = kanjis_of_the_day_df[["meanings","readings_on","readings_kun"]]
 
-    outcome = tabulate.tabulate(kanjis_of_the_day_df, tablefmt="fancy_grid", headers="keys")
-    return outcome
-    #return kanji_and_meanings.to_string(header=False)
+    return kanji_and_meanings.to_string(header=False)
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import discord
 from discord.ext import commands, tasks
 
 import kanji_reader
@@ -8,7 +7,7 @@ import config as CONFIG
 bot = commands.Bot("!")
 todays_number = 1
 
-@tasks.loop(seconds=24)
+@tasks.loop(hours=24)
 async def called_once_a_day():
     channel_id = int(os.environ['TARGET_CHANNEL_ID']) # needs to be cast to int otherwise would return none
     message_channel = bot.get_channel(channel_id)
@@ -16,12 +15,7 @@ async def called_once_a_day():
     print(f" INFO: Sending day {todays_number} on channel {message_channel}")
     await message_channel.send("Kanji of day#"+str(todays_number))
     kanji_of_the_day_table_as_string = kanji_reader.generate_todays_kanji_(todays_number, CONFIG.KANJI_AMOUNT)
-
     await message_channel.send(kanji_of_the_day_table_as_string)
-    #kanji_embed = discord.Embed()
-    #kanji_embed.add_field(name="Kanji of day# - "+str(todays_number), value=, inline=True)
-    #kanji_embed.set_footer(text=kanji_of_the_day_html)
-    #await message_channel.send(embed=kanji_embed)
 
     todays_number = todays_number + 1
 

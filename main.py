cd659f7414c5759983affa3009a2886879b1aecd
Karim <abdulkareemx2@gmail.com>
2021-07-05 15:08:56 +0900

    added title msg + loops to 24h

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ async def called_once_a_day():
     channel_id = int(os.environ['TARGET_CHANNEL_ID']) # needs to be cast to int otherwise would return none
     message_channel = bot.get_channel(channel_id)
     global todays_number
-    print(f"Sending day {todays_number} on channel s{message_channel}")
-    await message_channel.send("Kanjis of day#"+str(todays_number))
+    print(f"Sending day {todays_number} on channel {message_channel}")
+    await message_channel.send("Kanji of day#"+str(todays_number))
     await message_channel.send(file=discord.File('Todays_Kanji.png'))
     kanji_of_the_day_markdown = kanji_reader.generate_todays_kanji_(todays_number, CONFIG.KANJI_AMOUNT)
     await message_channel.send(kanji_of_the_day_markdown)
     todays_number = todays_number + 1
+
 
 @called_once_a_day.before_loop
 async def before():

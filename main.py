a84b7e55469bd7f694f37d12e287997fea8f4f90
Karim <abdulkareemx2@gmail.com>
2021-07-05 18:37:42 +0900

    chaning mins to 1.0 and adding comments

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import config as CONFIG
 bot = commands.Bot("!")
 todays_number = 1
 
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=1.0)
 async def called_once_a_day():
     channel_id = int(os.environ['TARGET_CHANNEL_ID']) # needs to be cast to int otherwise would return none
     message_channel = bot.get_channel(channel_id)
     global todays_number
-    print(f"Sending day {todays_number} on channel {message_channel}")
+    print(f" INFO: Sending day {todays_number} on channel {message_channel}")
     await message_channel.send("Kanji of day#"+str(todays_number))
     await message_channel.send(file=discord.File('Todays_Kanji.png'))
     kanji_of_the_day_markdown = kanji_reader.generate_todays_kanji_(todays_number, CONFIG.KANJI_AMOUNT)
@@ -24,7 +24,8 @@ async def called_once_a_day():
 @called_once_a_day.before_loop
 async def before():
     await bot.wait_until_ready()
-    print("Bot is running")
+    print("INFO:  Bot is running")
 
 called_once_a_day.start()
+print("INFO: bot start run")
 bot.run(os.environ['DISCORD_BOT_TOKEN'])

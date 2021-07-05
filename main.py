9c645f9d64eebfb6dd4cd5473d60be5a787cc1ad
Karim <abdulkareemx2@gmail.com>
2021-07-05 15:44:44 +0900

    changing to mins

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import config as CONFIG
 bot = commands.Bot("!")
 todays_number = 1
 
-@tasks.loop(hours=24)
+@tasks.loop(minutes=1)
 async def called_once_a_day():
     channel_id = int(os.environ['TARGET_CHANNEL_ID']) # needs to be cast to int otherwise would return none
     message_channel = bot.get_channel(channel_id)
